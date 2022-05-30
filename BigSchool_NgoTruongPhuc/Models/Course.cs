8ca72cb3144312383e6bc5f15c63ec234ddf9582
Phuc Ngo <truongphuc246@gmail.com>
2022-05-30 17:48:31 +0700

    Thêm Course, Category

---
+++ BigSchool_NgoTruongPhuc/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_NgoTruongPhuc.Models
+{
+    public class Course
+    {   
+
+        public int ID { get; set; } 
+        public ApplicationUser Lecturer { get; set; }
+        [Required] 
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)] 
+        public string Place 
+        
+        
+    }
+}
