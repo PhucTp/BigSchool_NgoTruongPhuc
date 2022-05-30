0039d9f4e04cbb5b8bda4e9ffba148b1afc79688
truon <truon@FUCLING>
2022-05-30 17:25:46 +0700

    Add project files.

---
+++ BigSchool_NgoTruongPhuc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_NgoTruongPhuc.Startup))]
+namespace BigSchool_NgoTruongPhuc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
