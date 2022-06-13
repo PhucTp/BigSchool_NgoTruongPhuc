c1edf3415b42af3f7f0b7a2ab7f8b768e78c88ad
1 <123>
2022-06-13 16:34:48 +0700

    Đã làm lại sau khi bị lỗi, tới trang 16 ( 13/06/2002)

---
+++ BigSchool_NgoTruongPhuc/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool_NgoTruongPhuc.Models;
+using BigSchool_NgoTruongPhuc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool_NgoTruongPhuc.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
